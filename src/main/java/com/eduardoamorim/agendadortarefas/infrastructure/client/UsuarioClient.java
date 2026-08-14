5fe5beea1e2f76b1b1182bcd5a317e2555e151fe
Eduardo <eduardoamorim0005@gmail.com>
2026-08-14 00:52:41 -0300

    Adicionadas as configurações de segurança

---
+++ src/main/java/com/eduardoamorim/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -0,0 +1,19 @@
+package com.eduardoamorim.agendadortarefas.infrastructure.client;
+
+import com.eduardoamorim.agendadortarefas.business.dto.UsuarioDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "usuario", url = "${usuario.url}")
+
+public class UsuarioClient {
+
+    @GetMapping
+    UsuarioDTO buscaUsuarioPorEmail(@RequestParam("email") String email,
+                                    @RequestHeader("Authorization") String token) {
+        return null;
+    }
+}
